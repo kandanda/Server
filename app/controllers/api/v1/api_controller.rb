66fa3d06c185043aa3f2a497fce12ffd115ccfa3
Mike <mike@otep.ch>
2017-03-21 09:45:56 +0100

    add better handling for bad input on api EPJ-70

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,2 +1,3 @@
 class Api::V1::ApiController < ActionController::Base
+
 end
--- test/integration/tournament_api_test.rb
+++ test/integration/tournament_api_test.rb
@@ -1,23 +1,52 @@
 require 'test_helper'
 
 class TournamentApiTest < ActionDispatch::IntegrationTest
-  test "upload tournament results to api" do
+  def setup
     @organizer = Organizer.first
 
+  end
+
+  def login
     post "/api/v1/auth",
       params: {email: 'test@kandanda.ch', password: 'secret' }
 
-    token = JSON.parse(response.body)['auth_token']
+    @token = JSON.parse(response.body)['auth_token']
+  end
 
-    tempered_token = temper_with(token)
+  test "tournament upload with tempered token is rejected" do
+    login
+    tempered_token = temper_with(@token)
     post "/api/v1/tournaments",
       headers: {"Authorization" =>  "Bearer #{tempered_token}"},
       params: JSON.parse(File.read(File.join(Rails.root, "test/example_requests/push_tournament_1.json")))
-
     assert_redirected_to new_organizer_session_path
+  end
+
+  test "tournament upload with invalid params fails gracefully" do
+    login
+    post "/api/v1/tournaments",
+      headers: {"Authorization" =>  "Bearer #{@token}"},
+      params: {}
+    assert_response :unprocessable_entity
 
     post "/api/v1/tournaments",
-      headers: {"Authorization" =>  "Bearer #{token}"},
+      headers: {"Authorization" =>  "Bearer #{@token}"},
+      params: { tournament: {}}
+
+    post "/api/v1/tournaments",
+      headers: {"Authorization" =>  "Bearer #{@token}"},
+      params: { tournament: {name: ''}}
+    post "/api/v1/tournaments",
+      headers: {"Authorization" =>  "Bearer #{@token}"},
+      params: { tournament: {name: '', phases: [{from: Time.now}]}}
+    assert_response :unprocessable_entity
+  end
+
+
+  test "upload tournament results to api" do
+    login
+    post "/api/v1/tournaments",
+      headers: {"Authorization" =>  "Bearer #{@token}"},
       params: JSON.parse(File.read(File.join(Rails.root, "test/example_requests/push_tournament_1.json")))
 
     assert_response :success
