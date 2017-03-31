84f1c317ce120c1ec0f5390f27df43a3dc169c93
Mike <m3gerber@hsr.ch>
2017-03-31 10:06:59 +0200

    EPJ-91 add basic stats to dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,10 +3,11 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    div do
+      ul do
+        li "Organizers registered today: #{Organizer.where("created_at > ?", 1.day.ago).count}"
+        li "Tournaments created today: #{Tournament.where("created_at > ?", 1.day.ago).count}"
+        li "Tournaments updated today: #{Tournament.where("updated_at > ?", 1.day.ago).count}"
       end
     end
 
